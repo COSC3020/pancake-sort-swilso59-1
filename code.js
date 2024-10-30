84a74725fb9cfba0e12fe51d54e3dfaf8f078039
Sean W. <104464288+swilso59@users.noreply.github.com>
2024-10-30 15:52:50 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,33 @@
-function flip(array, n) {
-    return array;
+function pancakeSort(array) {
+    for (var i = array.length; i > 1; i--) {
+        var maxIndex = findMax(array, i);
+        
+        if (maxIndex != i - 1) {
+            flip(array, maxIndex);
+            flip(array, i - 1);
+        }
+    }
+    return array; // Return sorted array.
 }
 
-// Use only flip() here to manipulate the array
-function pancakeSort(array) {
-    return array;
+function flip(array, n) {
+    var start = 0;
+    while (start < n) {
+        var temp = array[start];
+        array[start] = array[n];
+        array[n] = temp;
+        start++;
+        n--;
+    }
+    return array; // Return modified array.
+}
+
+function findMax(array, n) {
+    var maxIndex = 0;
+    for (var i = 0; i < n; i++) {
+        if (array[i] > array[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+    return maxIndex; // Return index of max element.
 }
