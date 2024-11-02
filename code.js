073cd65d8e523257a1c6981ddba022ede5a3cac9
Sean W. <104464288+swilso59@users.noreply.github.com>
2024-11-02 17:14:02 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,45 +1,43 @@
 function pancakeSort(array) {
-    // Iterate from the end of the array to the start
-    for (var i = array.length; i > 1; i--) {
-        var maxIndex = findMax(array, i);
-        // if the maxIndex is not at the end of the array
-        if (maxIndex != i - 1) {
-            // Flips maxIndex tot the front then to the end 
-            flip(array, maxIndex);
+    // define maxIndex to find the index of the maximum element
+    const maxIndex = (arr, n) => {
+        let maxIdx = 0;
+        for (let j = 0; j < n; j++) {
+            if (arr[j] > arr[maxIdx]) {
+                maxIdx = j;
+            }
+        }
+        return maxIdx;
+    };
+    
+    // iterate from the end of the array down to the start
+    for (let i = array.length; i > 1; i--) {
+        // use the maxIndex to find the max element's index in the unsorted portion
+        let maxIdx = maxIndex(array, i);
+
+        // if the maximum element is not already in the correct position
+        if (maxIdx !== i - 1) {
+            // flip maxIndex to the front if it's not already there
+            if (maxIdx !== 0) {
+                flip(array, maxIdx);
+            }
+            // flip the maximum element to its correct position in the unsorted section
             flip(array, i - 1);
         }
     }
     return array; 
 }
 
-// Flips the order the the elements from the start to n in the array 
+// reverses the order of elements in the array from index 0 to index n
 function flip(array, n) {
-    var start = 0;
-    
+    let start = 0;
+    // swap elements from the start and end moving towards the middle
     while (start < n) {
-        // store element at start position
-        var temp = array[start];
-        // move element from n position to start 
+        let temp = array[start];
         array[start] = array[n];
-        //place temp element in n position
         array[n] = temp;
-        // Move start and n index
         start++;
         n--;
     }
     return array; 
-}
-
-// Find the index of the largest element within the first n elements
-function findMax(array, n) {
-    var maxIndex = 0;
-    // loop through the first n elements to find maxIndex
-    for (var i = 0; i < n; i++) {
-        // if the element in array[i] is greater than the element in array[maxIndex]
-        if (array[i] > array[maxIndex]) {
-            // assign element in position i to maxIndex 
-            maxIndex = i;
-        }
-    }
-    return maxIndex; 
 }
